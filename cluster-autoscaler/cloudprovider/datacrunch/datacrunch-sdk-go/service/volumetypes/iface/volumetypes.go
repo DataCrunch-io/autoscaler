890d0b8c5571fd3ef40d7f8dbe5a0fc1a179ce4b
Lei <lei@datacrunch.io>
2025-08-07 23:56:03 +0300

    datacrunch sdk update

--- cluster-autoscaler/cloudprovider/datacrunch/datacrunch-sdk-go/service/volumetypes/iface/volumetypes.go
+++ cluster-autoscaler/cloudprovider/datacrunch/datacrunch-sdk-go/service/volumetypes/iface/volumetypes.go
@@ -1,15 +1,13 @@
 package interfaces
 
 import (
-	"context"
-
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/datacrunch/datacrunch-sdk-go/service/volumetypes"
 )
 
 // VolumeTypesAPI provides the interface for the volume types service
 type VolumeTypesAPI interface {
 	// ListVolumeTypes lists all available volume types
-	ListVolumeTypes(ctx context.Context) ([]*volumetypes.VolumeTypeResponse, error)
+	ListVolumeTypes() ([]*volumetypes.VolumeTypeResponse, error)
 }
 
 var _ VolumeTypesAPI = (*volumetypes.VolumeTypes)(nil)
